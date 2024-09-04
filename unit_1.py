951cc94b60270d2b3ae5f77900208dbd0be1f77d
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-09-04 15:10:27 +0000

    created a simple unittest

--- unit_1.py
+++ unit_1.py
@@ -1,9 +1,19 @@
 import unittest
 
+import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+def add(a, b):
+    return a + b
+
+
+class TestAddFunction(unittest.TestCase):
+
+    def test_add_integers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_strings(self):
+        self.assertEqual(add("hello ", "world"), "hello world")
 
 
 if __name__ == '__main__':
