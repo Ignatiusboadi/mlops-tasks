351ed8b9b416528c84e183c29a271580aa1a67ff
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-09-04 14:43:24 +0000

    fit model to data and made predictions

--- iris.py
+++ iris.py
@@ -25,5 +25,10 @@ y = iris_df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
 
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
 
 
